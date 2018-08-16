58d70285dd619226d21865f3dbaf06d00b46eaae
CanoMaria <oibanez311@alumnos.iua.edu.ar>
2018-08-16 09:55:22 -0300

    agregue el ejercicio1

---
+++ Recursividad/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,24 @@
+/*1) (Visualización de la recursión ) Es interesante ver “en acción” a la recursión.Modifique el método factrn ( factorial ) 
+para que imprima su variable local y el parámetro de llamada de la recursión. Por cada llamada recursiva, presente su salida
+en una línea independiente y agregue un nivel de sangría. Haga su mejor esfuerzo por hacer que la salida sea clara, 
+interesante y significativa. Su meta aquí es diseñar e implementar un formato de salida que ayude a la gente a entender 
+mejor la recursión. Tal vez desee agregar estas capacidades de presentación a los otros ejercicios del práctico.*/
+
+package ejercicio1;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ejercicio1 FACTORIAL");
+        System.out.println("Ingrese un numero:");
+        int nro = scanner.nextInt();
+       }
+    
+    int factorial(){
+    return 0;
+    }
+}
